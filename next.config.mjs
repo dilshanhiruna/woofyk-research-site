298af25b310afd6cb818c592b77325059ac8ae9a
dilshanhiruna <dilshan.hirunaa@gmail.com>
2023-05-21 00:27:41 +0530

    chore: remove unwanted files for static

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,25 @@ import remarkCodeTitles from './src/lib/remark-code-title.mjs'
 import rehypePresetMinify from 'rehype-preset-minify'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: false,
   pageExtensions: ['jsx', 'mdx'],
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     newNextLinkBehavior: true,
     scrollRestoration: true,
+  },
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/documents': { page: '/documents' },
+      '/presentations': { page: '/presentations' },
+      '/scope': { page: '/scope' },
+      '/milestones': { page: '/milestones' },
+      '/404': { page: '/404' },
+    }
   },
 }
 
@@ -19,7 +32,7 @@ const withMDX = nextMDX({
   options: {
     remarkPlugins: [remarkGfm, remarkCodeTitles],
     rehypePlugins: [rehypePrismPlus, rehypePresetMinify],
-        providerImportSource: '@mdx-js/react',
+    providerImportSource: '@mdx-js/react',
   },
 })
 
